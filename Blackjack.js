7236e58bc23e41abcb354e1f29c8936210d222a8
Carson Cook <ccook2018@francisparker.org>
2016-01-29 14:45:33 -0800

    hm

--- Blackjack.js
+++ Blackjack.js
@@ -1,95 +1,80 @@
-//dealer
 var canvasGA = document.getElementById("gameAreaDealer");
 // canvasGA.addEventListener( 'click', processUserInput);
 var contextGA = canvasGA.getContext("2d");
-//player
+
 var canvasGA2 = document.getElementById("gameAreaPlayer");
 // canvasGA2.addEventListener( 'click', processUserInput);
 var contextGA2 = canvasGA2.getContext("2d");
 
 var cardWidth = 122;
-var cardHeight = 174 ;
-var constant = 100;
-var x;
+var cardHeight = 174;
 var img = new Image();
 img.src = "deckOfCards1.jpg";
-var temp = 0;
-var amtCards = 0;
-var count = 0;
+img.onload = function() {
+  card(deck[nextRandomCard], true, "player");
+};
+
+var constant = 100;
+
+var deck = [];
 var usedCards = [];
+var playerHand = [];
+var dealerHand = [];
 
-setTimeout( setupTable, 1000);
+for(s = 0; s < 4; s++){
+  for(i = 1; i <= 13; i++){
+    deck.push(i + "" + s);
+  }
+}
 
-function randomCard(x){
-  if(x == true){
-    var randV = Math.floor(Math.random()*13);
-    var randT = Math.floor(Math.random()*4);
-    var placement;
-    var blah = true;
-    var place = (600 - ((constant*1.426)+constant))/2;
-    console.log(randT);
-    placement = place + amtCards*cardWidth;
-    for(i = 0;i<usedCards.length;i++){
-      if(usedCards[i] == randV + "." + randT){
-        randV = Math.floor(Math.random()*13);
-        randT = Math.floor(Math.random()*4);
-        console.log("hit");
-        randomCard();
-      }
-    }
-    drawImagePlayer(randV, randT, placement, 50);
-    amtCards++;
-    count++;
-  }else if(x == false){
-    if(count == 2){
-      amtCards = 0;
-    }
-    var randV = Math.floor(Math.random()*13);
-    var randT = Math.floor(Math.random()*4);
-    var placement;
-    var blah = true;
-    var place = (600 - ((constant*1.426)+constant))/2;
-    console.log(randT);
-    placement = place + amtCards*cardWidth;
-    for(i = 0;i<usedCards.length;i++){
-      if(usedCards[i] == randV + "." + randT){
-        randV = Math.floor(Math.random()*13);
-        randT = Math.floor(Math.random()*4);
-        console.log("hit");
-        randomCard();
-      }
-    }
-    drawImageDealer(randV, randT, placement, 50);
-    amtCards++;
-    count++;
+var nextRandomCard = getRandomCard();
+
+function card(currCard, shown, side){
+  //player
+  if(side == "player"){
+    playerHand.push(currCard);
+    drawImagePlayer(currCard, 100, 100);
+    //dealer
+  }else if(side == dealer){
+    dealerHand.push(currCard);
+    drawImageDealer(currCard, 50, 50);
+  }else{
   }
 };
 
-function setupTable(){
-  randomCard(true);
-  console.log(usedCards);
-  randomCard(true);
-  console.log(usedCards);
-  randomCard(false);
-  console.log(usedCards);
-  randomCard(false);
-  console.log(usedCards);
+function getRandomCard(){
+  num = Math.floor(Math.random() * deck.length);
+  console.log(deck[num]);
+  remove(deck, num);
+
+  console.log(deck);
+  return num;
 };
 
-function drawImageDealer(v, t, x, y){
+function remove(deck, card) {
+  return deck.slice(0, card).concat(deck.slice(card + 1));
+};
+
+function drawImageDealer(randomCard, x, y){
   var c = document.getElementById("gameAreaDealer");
   var ctx = c.getContext("2d");
-  var clipX = 2 + ((v*10.2)+ (cardWidth * v));
-  var clipY = 2 + ((t * 11.35) + (cardHeight * t));
+  var suite = randomCard.substring(randomCard.length - 1, randomCard.length);
+  var num = randomCard.substring(0, randomCard.length - 1);
+  dealerHand.push(num + " " + suite);
+  var clipX = 2 + ((num*10.2) + (cardWidth * num));
+  var clipY = 2 + ((suite * 11.35) + (cardHeight * suite));
+  console.log(" clipX: " + clipX + " clipY: " + clipY + " cardWidth: " + cardWidth + " cardHeight: " + cardHeight + " x: " + x + " y: " + y + " constant: " + constant);
   ctx.drawImage(img, clipX, clipY, cardWidth, cardHeight, x, y, constant, constant*1.426);
-  usedCards.push(v + "." + t);
 };
 
-function drawImagePlayer(v, t, x, y){
+function drawImagePlayer(randomCard, x, y){
   var c = document.getElementById("gameAreaPlayer");
   var ctx = c.getContext("2d");
-  var clipX = 2 + ((v*10.2)+ (cardWidth * v));
-  var clipY = 2 + ((t * 11.35) + (cardHeight * t));
+  var suite = randomCard.substring(randomCard.length - 1, randomCard.length);
+  var num = randomCard.substring(0, randomCard.length - 1);
+  playerHand.push(num + " " + suite);
+  var clipX = 2 + ((num*10.2) + (cardWidth * num));
+  var clipY = 2 + ((suite * 11.35) + (cardHeight * suite));
+  console.log(" clipX: " + clipX + " clipY: " + clipY + " cardWidth: " + cardWidth + " cardHeight: " + cardHeight + " x: " + x + " y: " + y + " constant: " + constant);
   ctx.drawImage(img, clipX, clipY, cardWidth, cardHeight, x, y, constant, constant*1.426);
-  usedCards.push(v + "." + t);
 };
