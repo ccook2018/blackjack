dc59be6e2b42a1b92514fd7d0d78a4b2d171e426
Carson Cook <ccook2018@francisparker.org>
2016-02-02 08:38:21 -0800

    please

--- Blackjack.js
+++ Blackjack.js
@@ -1,38 +1,42 @@
-var canvasGA = document.getElementById("gameAreaDealer");
-// canvasGA.addEventListener( 'click', processUserInput);
-var contextGA = canvasGA.getContext("2d");
+//BLACKJACK
 
-var canvasGA2 = document.getElementById("gameAreaPlayer");
-// canvasGA2.addEventListener( 'click', processUserInput);
-var contextGA2 = canvasGA2.getContext("2d");
-
-var cardWidth = 122;
-var cardHeight = 174;
+//the image cards are pulled from
 var img = new Image();
 img.src = "deckOfCards1.jpg";
+
+//allows image to load before trying to access it
 img.onload = function() {
   card(deck[nextRandomCard], true, "player");
 };
 
+//used as a constant variable for card shown in game
 var constant = 100;
 
+//dimensions of card on image
+var cardWidth = 122;
+var cardHeight = 174;
+
+//used to track cards that are still in deck and who has what cards
 var deck = [];
-var usedCards = [];
 var playerHand = [];
 var dealerHand = [];
 
+//fills deck with "cards"
 for(s = 0; s < 4; s++){
   for(i = 1; i <= 13; i++){
     deck.push(i + "" + s);
   }
 }
 
+//prepares next random card number
 var nextRandomCard = getRandomCard();
 
+//shows the random card
 function card(currCard, shown, side){
   //player
   if(side == "player"){
     playerHand.push(currCard);
+    console.log(currCard);
     drawImagePlayer(currCard, 100, 100);
     //dealer
   }else if(side == dealer){
@@ -42,19 +46,21 @@ function card(currCard, shown, side){
   }
 };
 
+//returns random number within the deck
 function getRandomCard(){
   num = Math.floor(Math.random() * deck.length);
   console.log(deck[num]);
   remove(deck, num);
-
   console.log(deck);
   return num;
 };
 
+//removes chosen random card from deck
 function remove(deck, card) {
   return deck.slice(0, card).concat(deck.slice(card + 1));
 };
 
+//shows the card(s) dealt to the dealer
 function drawImageDealer(randomCard, x, y){
   var c = document.getElementById("gameAreaDealer");
   var ctx = c.getContext("2d");
@@ -67,6 +73,7 @@ function drawImageDealer(randomCard, x, y){
   ctx.drawImage(img, clipX, clipY, cardWidth, cardHeight, x, y, constant, constant*1.426);
 };
 
+//shows the card(s) dealt to the player
 function drawImagePlayer(randomCard, x, y){
   var c = document.getElementById("gameAreaPlayer");
   var ctx = c.getContext("2d");
